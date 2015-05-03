d52ce219cd275d37aeb1e63fd0a25e0f07f86150
maryk.december@gmail.com <maryk.december@gmail.com>
2015-05-03 11:14:53 -0500

    get similar tracks on click

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
 
     var username= 'marydecember_';
     var api_key = 'd43908568d7b06a5c4d049def66ff619';
-    var track = "Work it out";
+    var track;
     var trackResults;
-    var artist = "Velveteens";
+    var artist;
 
 
     $.ajax({
@@ -14,21 +14,35 @@ $(document).ready(function () {
         crossDomain: true,
         dataType: 'json',
         success: function (data) {
-            console.log(data);
+            track = data.recenttracks.track[0].name;
+            artist = data.recenttracks.track[0].artist['#text'];
             $('.recentTrack').text(data.recenttracks.track[0].name);
             $('.recentArtist').text(data.recenttracks.track[0].artist['#text']);
             $('.recentImg').attr('src', data.recenttracks.track[0].image[2]['#text']);
             $('.recentAlbum').text(data.recenttracks.track[0].album['#text']);
-            console.log(data.recenttracks.track[0].name);
-            console.log(data.recenttracks.track[0].album['#text']);
-            console.log(data.recenttracks.track[0].artist['#text']);
-            console.log(data.recenttracks.track[0].image[2]['#text']);
 
         },
         error: function (xhr, status) {
             alert('Error: ' + status);
         }
+    });
 
+
+    $('.recentImg').click(function() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://ws.audioscrobbler.com/2.0?method=track.getsimilar&artist=' + encodeURI(artist) + '&track=' + track + '&api_key=d43908568d7b06a5c4d049def66ff619&format=json',
+            crossDomain: true,
+            dataType: 'json',
+            success: function (data) {
+                console.log('Hello');
+                console.log(data);
+
+            },
+            error: function (xhr, status) {
+                alert('Error: ' + status);
+            }
+        });
     });
 
     //$.ajax({
